a29e2cc1866ee8d8c486c481ba72d893280b861d
JacquelineCuenca <jacuenca@espol.edu.ec>
2025-11-14 16:54:26 -0500

    codigo archivos .h

--- productos.h
+++ productos.h
@@ -1,1 +1,16 @@
+#ifndef PRODUCTOS_H
+#define PRODUCTOS_H
+
+typedef struct {
+    int codigo;
+    char nombre[50];
+    int cantidad;
+    float costo;
+    float precioVenta;
+} Producto;
+
+int cargarProductos(Producto **productos);
+int actualizarInventario(Producto *productos, int numProductos);
+
+#endif
 
--- usuarios.h
+++ usuarios.h
@@ -1,1 +1,15 @@
+#ifndef USUARIOS_H
+#define USUARIOS_H
+
+typedef struct {
+    char usuario[30];
+    char clave[30];
+    char nombre[50];
+} Usuario;
+
+int login(Usuario *usuarios, int numUsuarios);
+
+int cargarUsuarios(Usuario **usuarios);
+
+#endif
 
